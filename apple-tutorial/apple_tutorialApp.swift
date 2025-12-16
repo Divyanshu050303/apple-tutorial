65e4f8b30ea325e33132b44e170698e050f2a413
divyanshuAntino <divyanshu@antino.com>
2025-12-16 12:23:35 +0530

    Initial Commit

---
+++ apple-tutorial/apple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_tutorialApp.swift
+//  apple-tutorial
+//
+//  Created by RAC IT on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct apple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apple-tutorialTests/apple_tutorialTests.swift
@@ -0,0 +1,16 @@
+//
+//  apple_tutorialTests.swift
+//  apple-tutorialTests
+//
+//  Created by RAC IT on 16/12/25.
+//
+
+import Testing
+
+struct apple_tutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
